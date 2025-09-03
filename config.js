e3489813f3a9035c8e30a93c6ccdb14337b54c39
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-09-04 02:00:56 +0530

    fixed mongodb connection and update package manager

--- config.js
+++ config.js
@@ -9,8 +9,6 @@ export const db = {
   name: process.env.DB_NAME || "",
   host: process.env.DB_HOST || "",
   port: process.env.DB_PORT || "",
-  user: process.env.DB_USER || "",
-  password: process.env.DB_USER_PWD || "",
 }
 
 export const corsUrl = process.env.CORS_URL
